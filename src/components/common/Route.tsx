e48bde1687d04e10228d55807eb0c4dd31c03ed3
minsuKang <minsu910725@gmail.com>
2023-07-05 12:49:08 +0900

    Feat: 라우터 및 라우트 컴포넌트 완성

---
+++ src/components/common/Route.tsx
@@ -0,0 +1,17 @@
+// Route.tsx
+import React, { ReactNode, useContext } from 'react';
+import { RouterContext } from './Router';
+
+interface RouteProps {
+  path: string;
+  component: ReactNode;
+}
+
+const Route: React.FC<RouteProps> = ({ path, component: Component }) => {
+  const { currentPath } = useContext(RouterContext);
+  const pathMatch = new RegExp(`^${path}$`).test(currentPath);
+
+  return pathMatch ? Component : null;
+};
+
+export default Route;
